6d1402984671bdedab590197bf819d57e60c8b5b
김동호 <68005740+KimDongHo-Iisan@users.noreply.github.com>
2021-07-22 00:22:55 +0900

    1

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -13,12 +13,16 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        print(searchTextField.text!) //강제 언래핑을 하지 않으면, 안에 있는 값을 출력할수 없음, 이유는 text안이 비어있을 가능성도 있기 때문.
+    }
+    
 }
 
